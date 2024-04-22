93f6930245591da36a51fbf5a72c848b5262b829
kamsiokonji <kamsi.okonji9@gmail.com>
2024-04-22 09:19:23 +0100

    added search and filter functionality

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Header from "../Header/Header";
-import CountryPicker from "../CountryPicker/CountryPicker";
-import Cards from "../Cards/Cards";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <>
-      <div className="dark:bg-[#1a2c3a] bg-[#fafafa] text-[#111517] dark:text-white">
+      <div className="h-screen dark:bg-[#1a2c3a] bg-[#fafafa] text-[#111517] dark:text-white">
         <Header />
-      </div>
 
-      <div className="h-[100&] lg:px-20 sm:py-10 sm:px-4 flex flex-col space-y-14 dark:bg-[#1a2c3a] bg-[#fafafa] text-[#111517] dark:text-white">
-        <CountryPicker />
+        <div className="lg:px-20 sm:py-10 sm:px-4 flex flex-col space-y-14 dark:bg-[#1a2c3a] bg-[#fafafa] text-[#111517] dark:text-white">
+          <Outlet />
+        </div>
       </div>
     </>
   );
